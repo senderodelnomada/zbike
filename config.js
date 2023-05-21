30bf0848d056ca567c44335caaaa41a9d16c595b
senderodelnomada <134119492+senderodelnomada@users.noreply.github.com>
2023-05-21 03:27:09 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO  //
-<!-- const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec"; -->
+<!-- const AS_URL_BASE = "https://script.google.com/macros/s/*****AquiVaTuURL****/exec"; -->
 
 const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbz0vWyqvckkOY2zIGWS7JWQe0qI1GIumHVqhKeVOyKu6G87myg_IjuTPSYfi3TVRjmX/exec";
